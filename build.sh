93bd1ac6fd32c268918915888332a986ff6892d4
Eduardo Costa <m4c0@users.noreply.github.com>
2026-06-24 21:24:47 +0100

    zipalign

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ AJARDIR=$ANDROID_SDK_ROOT/platforms/android-36-ext19
 BASEDIR=$ANDROID_NDK/toolchains/llvm/prebuilt/linux-x86_64
 RES_DIR=$BASEDIR/lib/clang/18
 SYSROOT=$BASEDIR/sysroot
+TOOLDIR=$ANDROID_SDK_ROOT/build-tools/37.0.0
 
 FLAGS="-shared main.c -Wl,-Bsymbolic -fuse-ld=lld -Wl,--no-undefined -llog -resource-dir $RES_DIR --sysroot $SYSROOT"
 
@@ -20,13 +21,15 @@ $BASEDIR/bin/armv7a-linux-androideabi32-clang $FLAGS -o app/armeabi-v7a/libhello
 $BASEDIR/bin/i686-linux-android32-clang       $FLAGS -o app/x86/libhello.so         --target=i686-linux-android32
 $BASEDIR/bin/x86_64-linux-android32-clang     $FLAGS -o app/x86_64/libhello.so      --target=x86_64-linux-android32
 
-AAPT2=$ANDROID_SDK_ROOT/build-tools/37.0.0/aapt2
-$AAPT2 compile res/values/strings.xml -o .
-$AAPT2 link values_strings.arsc.flat -o app.res.apk --manifest AndroidManifest.xml -I $AJARDIR/android.jar
+$TOOLDIR/aapt2 compile res/values/strings.xml -o .
+$TOOLDIR/aapt2 link values_strings.arsc.flat -o app.res.apk --manifest AndroidManifest.xml -I $AJARDIR/android.jar
 
 cd app
 unzip -v ../app.res.apk
 zip -v -r ../app.unaligned.apk .
 cd -
 
-find app
+$TOOLDIR/zipalign -p -f -v 4 app.unaligned.apk app.apk
+
+file app.apk
+unzip -t app.apk
